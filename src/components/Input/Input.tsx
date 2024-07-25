c14b13b6fd57510992ebef4e2dd56b5f59ee93f0
AlinaIone_lia <alina.barbu.ionelia06@gmail.com>
2024-07-25 14:20:35 +0300

    Implement live search on input responsable with the number of users Fix the scroll oh the page after fetching data

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -6,39 +6,24 @@ interface Props {
   inputValue: string;
   placeholder?: string;
   errMessage: string;
-  handleFetchUsers?: () => void;
 }
 
-export const Input: FC<Props> = ({
-  verifyValue,
-  inputValue,
-  placeholder,
-  errMessage,
-  handleFetchUsers
-}) => {
+export const Input: FC<Props> = ({ verifyValue, inputValue, placeholder, errMessage }) => {
   const [isValid, setIsValid] = useState<boolean>(true);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    const checkValidation = verifyValue(value);
+    const checkValidation = verifyValue(e.target.value);
     setIsValid(checkValidation);
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
- if (e.key === 'Enter' && handleFetchUsers) {
-      handleFetchUsers();
-    }
   };
 
   return (
     <div style={{position: "relative"}}>
       <StyledInput
-       id="inputField"
+        id="inputField"
         type="text"
         value={inputValue}
         onChange={handleInputChange}
         placeholder={placeholder}
-        onKeyDown={handleKeyDown}
       />
       {!isValid && <WarningMessage> {errMessage} </WarningMessage>}
     </div>
--- src/components/UserFilters/UserFilters.tsx
+++ src/components/UserFilters/UserFilters.tsx
@@ -1,18 +1,22 @@
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState, useEffect, useMemo, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, storeActions, RootState } from "../../store/store";
 import { fetchUsers } from "../../store/slices";
 import { Input } from "../Input/Input";
 import { Button } from "../Button/Button";
+import debounce from "lodash.debounce";
 
 export const UserFilters = () => {
   const [usersNumberValue, setInputValue] = useState<string>("");
   const [searchValue, setSearchValue] = useState<string>("");
+  const [isFirstFetch, setIsFirstFetch] = useState(true);
+
+  const didMountRef = useRef(false);
+
   const dispatchUsers = useDispatch<AppDispatch>();
   const numberOfUsers = useSelector((store: RootState) => store.users.numberOfUsers);
-  const users = useSelector((store: RootState) => store.users.usersInitiallyFetched)
-  const usersList = useSelector((store: RootState) => store.users.users)
-
+  const users = useSelector((store: RootState) => store.users.usersInitiallyFetched);
+  const usersList = useSelector((store: RootState) => store.users.users);
 
   const verifyUsersNumberValue = (value: string) => {
     if (value === "" || /^[1-9]\d*$/.test(value)) {
@@ -28,61 +32,71 @@ export const UserFilters = () => {
     } else return false;
   };
 
-  const handleFetchOnClick = useCallback(() => {
-    if (usersNumberValue) {
-      // Handling sync state update via normal reducers
-      dispatchUsers(storeActions.users.setUsersNumber(Number(usersNumberValue)));
-      const requestArg = { usersNr: Number(usersNumberValue) };
-
-      // Handling async state update via thunk function (and extraReducers)
-      dispatchUsers(fetchUsers(requestArg));    
+  useEffect(() => {
+    if (didMountRef.current) {
+      if (!isFirstFetch && usersList && usersList.length > 0) {
+        window.scrollTo(0, 500);
+      }
+    } else {
+      didMountRef.current = true;
     }
-  }, [usersNumberValue, dispatchUsers]);
+  }, [usersList, isFirstFetch]);
+
+  const debouncedFetchUsers = useMemo(() =>
+      debounce((value: string) => {
+        let requestArg: { usersNr?: number; word?: string } = {};
+
+        if (!isNaN(Number(value))) {
+          requestArg.usersNr = Number(value);
+        } else {
+          requestArg.word = value;
+          requestArg.usersNr = Number(usersNumberValue) || numberOfUsers;
+        }
+
+        dispatchUsers(fetchUsers(requestArg));
+        setIsFirstFetch(false);
+      }, 300),
+    [dispatchUsers, numberOfUsers, usersNumberValue]
+  );
 
   useEffect(() => {
-    if (usersList &&  usersList.length > 0) {
-      window.scrollTo(0, 508);
+    if (usersNumberValue) {
+      debouncedFetchUsers(usersNumberValue);
     }
-  }, [ usersList]);
+  }, [usersNumberValue, debouncedFetchUsers]);
 
   useEffect(() => {
     if (searchValue) {
-      const requestArg = { usersNr: numberOfUsers, word: searchValue };
-      dispatchUsers(fetchUsers(requestArg));
+      debouncedFetchUsers(searchValue);
     }
-  }, [searchValue,dispatchUsers, numberOfUsers]);
+  }, [searchValue, debouncedFetchUsers]);
 
-  const clearInputs = useCallback(()=>{
-    setInputValue("")
-    setSearchValue("")
-    dispatchUsers(storeActions.users.setUsersList(users))
-  },[dispatchUsers, users])
-  
+  const clearInputs = useCallback(() => {
+    setInputValue("");
+    setSearchValue("");
+    dispatchUsers(storeActions.users.setUsersList(users));
+  }, [dispatchUsers, users]);
 
   return (
-    <div  style={{ display: "flex", justifyContent:'space-evenly', padding:'16px 24px' }}>
-      <div style={{ display: "flex" }}>
-        <Input
-          verifyValue={verifyUsersNumberValue}
-          inputValue={usersNumberValue}
-          placeholder={"Number of users"}
-          errMessage=" Only numeric values greater than 0 are allowed."
-          handleFetchUsers={handleFetchOnClick}
-        />
-        <Button
-          ariaLabel="apply-number"
-          variant="secondary"
-          children="Apply"
-          onClick={handleFetchOnClick}
-        />
-      </div>
+    <div style={{display: "flex", justifyContent: "space-evenly", padding: "16px 24px"}}>
+      <Input
+        verifyValue={verifyUsersNumberValue}
+        inputValue={usersNumberValue}
+        placeholder={"Number of users"}
+        errMessage=" Only numeric values greater than 0 are allowed."
+      />
       <Input
         placeholder="Search user"
         verifyValue={verifySearchValue}
         inputValue={searchValue}
         errMessage="Please enter only letters."
       />
-      <Button ariaLabel='clear' variant='secondary' children='Clear inputs' onClick={clearInputs}/>
+      <Button
+        ariaLabel="clear"
+        variant="secondary"
+        children="Clear inputs"
+        onClick={clearInputs}
+      />
     </div>
   );
 };
